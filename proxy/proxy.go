80d8e0f9ccab113f8290b6e05db25242ec768697
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-06-25 17:00:14 +0800

    feat:try to build the proxy.

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,3 +1,27 @@
 package proxy
 
-func main() {}
+import (
+	"net"
+	"runtime"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	res, err := net.Dial("tcp")
+}
+
+func main() {
+	runtime.GOMAXPROCS(2)
+	ln, err := net.Listen("tcp", ":24625")
+	if err != nil {
+		panic(err)
+	}
+	defer ln.Close()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handleConnection(conn)
+	}
+}
