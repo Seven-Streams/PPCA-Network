60f52a38ffb095f3d3b0b07baf37f7a6261d1955
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-06-27 09:30:39 +0800

    feat:add the gitignore and finish the part of client.

--- client/main.go
+++ client/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"net"
 	"runtime"
 )
 
-func handleConnection(conn net.Conn) {
+func handleClient(conn net.Conn) {
 	defer conn.Close()
 	buffer := make([]byte, 1024)
+	remote_buffer := make([]byte, 1024)
 	n, err := conn.Read(buffer)
 	if err != nil {
 		return
@@ -20,57 +20,41 @@ func handleConnection(conn net.Conn) {
 	}
 	response := []byte{0x05, 0x00}
 	conn.Write(response)
-	n, err = conn.Read(buffer)
+	remote_conn, err := net.Dial("TCP", "127.0.0.1:24625") //dial to the server.
 	if err != nil {
 		return
 	}
-	if buffer[0] != 0x05 {
-		panic("Unsupported SOCKS version")
-	}
-	if buffer[1] != 0x01 {
-		panic("Unsupported command")
-	}
-	if buffer[2] != 0x00 {
-		panic("Unsupported reserved field")
-	}
-	var host string
-	if buffer[3] == 0x01 {
-		ip := buffer[4:8]
-		host = fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
-	} else if buffer[3] == 0x03 {
-		host = string(buffer[5 : n-2])
-	} else if buffer[3] == 0x04 {
-		ipv6Bytes := buffer[4:20]
-		host = fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x",
-			ipv6Bytes[0], ipv6Bytes[1], ipv6Bytes[2], ipv6Bytes[3],
-			ipv6Bytes[4], ipv6Bytes[5], ipv6Bytes[6], ipv6Bytes[7],
-			ipv6Bytes[8], ipv6Bytes[9], ipv6Bytes[10], ipv6Bytes[11],
-			ipv6Bytes[12], ipv6Bytes[13], ipv6Bytes[14], ipv6Bytes[15])
-	}
-	port := int(buffer[n-2])<<8 | int(buffer[n-1])
-	fmt.Printf(fmt.Sprintf("%s:%d\n", host, port))
-	new_conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
+	remote_conn.Write([]byte(data))
+	_, err = remote_conn.Read(remote_buffer)
 	if err != nil {
 		return
 	}
-	localAddr := new_conn.LocalAddr().(*net.TCPAddr)
-	localPort := localAddr.Port
-	firstByte := byte(localPort >> 8)
-	secondByte := byte(localPort & 0xFF)
-	defer new_conn.Close()
-	response = []byte{0x05, 0x00, 0x00, 0x01, 0x7f, 0x00, 0x00, 0x01, firstByte, secondByte}
-	fmt.Println(response)
-	_, err = conn.Write(response)
+	conn.Write(remote_buffer) //negotiation.
+	buffer = buffer[:0]
+	remote_buffer = remote_buffer[:0]
+	_, err = conn.Read(buffer) // get the request.
 	if err != nil {
 		return
 	}
-	go io.Copy(new_conn, conn)
-	io.Copy(conn, new_conn)
+	_, err = remote_conn.Write(buffer) // pass the request.
+	if err != nil {
+		return
+	}
+	_, err = remote_conn.Read(remote_buffer) // get the reply.
+	if err != nil {
+		return
+	}
+	_, err = conn.Write(remote_buffer) // pass the reply.
+	if err != nil {
+		return
+	}
+	go io.Copy(remote_conn, conn)
+	io.Copy(conn, remote_conn)
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	ln, err := net.Listen("tcp", ":24625") //listen on port 24625
+	ln, err := net.Listen("tcp", ":24626") //listen on port 24626
 	if err != nil {
 		panic(err)
 	}
@@ -80,6 +64,6 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		go handleConnection(conn)
+		go handleClient(conn)
 	}
 }
