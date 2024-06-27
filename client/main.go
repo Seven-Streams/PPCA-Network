c8ce467e158b312cb60aca08457c13f0163dec21
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-06-27 09:55:59 +0800

    feat:finish client.

--- client/main.go
+++ client/main.go
@@ -8,43 +8,8 @@ import (
 
 func handleClient(conn net.Conn) {
 	defer conn.Close()
-	buffer := make([]byte, 1024)
-	remote_buffer := make([]byte, 1024)
-	n, err := conn.Read(buffer)
-	if err != nil {
-		return
-	}
-	data := string(buffer[:n])
-	if data[0] != 0x05 {
-		panic("Unsupported SOCKS version")
-	}
-	response := []byte{0x05, 0x00}
-	conn.Write(response)
-	remote_conn, err := net.Dial("TCP", "127.0.0.1:24625") //dial to the server.
-	if err != nil {
-		return
-	}
-	remote_conn.Write([]byte(data))
-	_, err = remote_conn.Read(remote_buffer)
-	if err != nil {
-		return
-	}
-	conn.Write(remote_buffer) //negotiation.
-	buffer = buffer[:0]
-	remote_buffer = remote_buffer[:0]
-	_, err = conn.Read(buffer) // get the request.
-	if err != nil {
-		return
-	}
-	_, err = remote_conn.Write(buffer) // pass the request.
-	if err != nil {
-		return
-	}
-	_, err = remote_conn.Read(remote_buffer) // get the reply.
-	if err != nil {
-		return
-	}
-	_, err = conn.Write(remote_buffer) // pass the reply.
+	remote_conn, err := net.Dial("tcp", "127.0.0.1:24625") //dial to the server.
+	defer remote_conn.Close()
 	if err != nil {
 		return
 	}
