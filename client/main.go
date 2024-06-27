80c29d64bbc1011d53611bc9b66b7288cd859c28
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-06-27 09:56:42 +0800

    fix:change the order.

--- client/main.go
+++ client/main.go
@@ -9,10 +9,10 @@ import (
 func handleClient(conn net.Conn) {
 	defer conn.Close()
 	remote_conn, err := net.Dial("tcp", "127.0.0.1:24625") //dial to the server.
-	defer remote_conn.Close()
 	if err != nil {
 		return
 	}
+	defer remote_conn.Close()
 	go io.Copy(remote_conn, conn)
 	io.Copy(conn, remote_conn)
 }
